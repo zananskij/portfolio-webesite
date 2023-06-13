bc01a89a978c5b9761d724493da28d5cdd6b63b4
zananskij <zananskij@gmail.com>
2023-06-12 20:52:18 -0400

    updated project sections images + links

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,8 +1,12 @@
-import React from "react"
+"use client" // this is a client component
+import React, { useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import SlideUp from "./SlideUp"
 import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+import { Carousel } from "react-responsive-carousel"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import Swipe from "react-easy-swipe"
 
 interface LinkWithLabelProps {
   href: string
@@ -41,7 +45,7 @@ const projects = [
     ],
     description:
       "An interactive, real-time chat application built with the MERN stack, featuring WebSockets for immediate messaging, secure user authentication using JWT and bcrypt for password hashing, all fully responsive and styled with Tailwind CSS.",
-    image: "/chatApp.png",
+    images: ["/chatter.png", "/chatterlogin.png"],
     githubFrontend: "https://github.com/zananskij/chatter-client",
     githubBackend: "https://github.com/zananskij/chatter-api",
     link: "https://chatter-messenger.herokuapp.com/",
@@ -60,17 +64,17 @@ const projects = [
     ],
     description:
       "An intuitive e-commerce application featuring an interactive shopping cart and secure checkout via Stripe integration. Built with JavaScript, React, and Express, it utilizes Axios for efficient data handling. The mobile-friendly design is crafted with CSS, ensuring a user-friendly shopping experience.",
-    image: "/travelblog.png",
+    images: ["/ecommerceMain.png", "/ecommerceProduct.png", "/ecommerceCart.png", "/ecommerceStripe.png"],
     githubFrontend: "https://github.com/zananskij/simple-ecommerce",
     githubBackend: "https://github.com/zananskij/simple-ecommerce-server",
     link: "https://simple-ecommerce.herokuapp.com/",
   },
   {
-    name: "Streaming backlist",
+    name: "Streaming Backlist",
     skillsUsed: ["TypeScript", "React", "Express", "PostgreSQL", "Node.js", "Axios", "Tailwind CSS", "Responsive"],
     description:
       "Explore a vast library of movies and TV shows, powered by the TMDB API. Discover trending titles or search for specific ones, each with detailed information including release dates, user ratings, and overviews. Users can curate a Favorites list to catalog titles they've enjoyed, or plan ahead with a Watchlater list for future viewing. Fully responsive and optimized, this application delivers an exceptional browsing experience on devices of all sizes.",
-    image: "/letterboxd.png",
+    images: ["/streamingHome.png", "/streamingModal.png"],
     githubFrontend: "https://github.com/zananskij/Letterboxd-clone-frontend",
     githubBackend: "https://github.com/zananskij/Letterboxd-clone-backend",
     link: "https://letterboxd-clone.herokuapp.com/",
@@ -80,19 +84,40 @@ const projects = [
     skillsUsed: ["TypeScript", "React", "Next.js", "Tailwind CSS", "Responsive"],
     description:
       "My personal portfolio, built with Next.js, highlights my design-focused approach to web development and my proficiency in creating interactive, responsive UI/UX. Demonstrating my ability to pick up and learn a new technology, while also leveraging my skills in TypeScript and React, it serves as an example to my continuous growth and future-focused mindset.",
-    image: "/letterboxd.png",
+    images: ["/portfolio1.png", "/portfolio2.png"],
     githubFrontend: "https://github.com/zananskij/portfolio-website",
-    githubBackend: "https://github.com/zananskij/Letterboxd-clone-backend",
     link: "https://jonzananski.herokuapp.com/",
   },
 ]
 
 const Projects = () => {
+  const sliderSettings = {
+    showArrows: true,
+    showStatus: false,
+    infiniteLoop: true,
+    showThumbs: false,
+    autoPlay: true,
+    interval: 3000,
+    transitionTime: 350,
+  }
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % projects[currentSlide].images.length)
+  }
+
+  const previousSlide = () => {
+    setCurrentSlide(
+      (prevSlide) => (prevSlide - 1 + projects[currentSlide].images.length) % projects[currentSlide].images.length
+    )
+  }
+
   return (
     <section id="projects">
       <h1 className="my-10 text-center font-bold text-4xl">
         Projects
-        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded" />
       </h1>
 
       <div className="flex flex-col space-y-28">
@@ -100,17 +125,26 @@ const Projects = () => {
           return (
             <div key={idx}>
               <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
-                    <Link href={project.link}>
-                      <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                      />
-                    </Link>
+                <div className="flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                  <div className="md:w-1/2">
+                    <Swipe onSwipeLeft={nextSlide} onSwipeRight={previousSlide}>
+                      <Carousel {...sliderSettings} selectedItem={currentSlide}>
+                        {project.images?.map((image, imageIdx) => (
+                          <div key={imageIdx}>
+                            <Link href={project.link}>
+                              <Image
+                                src={image}
+                                alt=""
+                                width={600}
+                                height={600}
+                                className="rounded-xl shadow-xl hover:opacity-70"
+                                style={{ width: "100%", height: "auto" }}
+                              />
+                            </Link>
+                          </div>
+                        ))}
+                      </Carousel>
+                    </Swipe>
                   </div>
                   <div className="mt-8 md:w-1/2">
                     <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
@@ -133,15 +167,18 @@ const Projects = () => {
                       <LinkWithLabel
                         href={project.githubFrontend}
                         IconComponent={BsGithub}
-                        label="Frontend Repo"
+                        label={project.githubBackend ? "Frontend Repo" : "Repo Here"}
                         className="hover:-translate-y-1 transition-transform cursor-pointer"
                       />
-                      <LinkWithLabel
-                        href={project.githubBackend}
-                        IconComponent={BsGithub}
-                        label="Backend Repo"
-                        className="hover:-translate-y-1 transition-transform cursor-pointer"
-                      />
+                      {project.githubBackend && (
+                        <LinkWithLabel
+                          href={project.githubBackend}
+                          IconComponent={BsGithub}
+                          label="Backend Repo"
+                          className="hover:-translate-y-1 transition-transform cursor-pointer"
+                        />
+                      )}
+
                       <LinkWithLabel
                         href={project.link}
                         IconComponent={BsArrowUpRightSquare}
